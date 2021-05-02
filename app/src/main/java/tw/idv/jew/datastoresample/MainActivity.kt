95b4ecdf08921940fa98eb9874e97c3ace4b559f
a0916105 <whdofjew@gmail.com>
2021-05-02 09:12:31 +0800

    AndroidX SharedPreferences

---
+++ app/src/main/java/tw/idv/jew/datastoresample/MainActivity.kt
@@ -0,0 +1,24 @@
+package tw.idv.jew.datastoresample
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.preference.PreferenceManager
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        //AndroidX SharedPreferences
+        val preferences = PreferenceManager.getDefaultSharedPreferences(this)
+        var value = preferences.getInt("MyKey", 0)
+
+        val success = preferences.edit()    //取得寫入權限
+                //設定修改資料
+                .putInt("MyKey", value)
+                .putInt("MyKey2", value+1)
+                //將資料寫入disk，並回傳是否成功
+                .commit()
+                //.apply()  //先修改記憶體中的資料，再非同步地寫入disk，沒有回傳值可以確認是否成功
+    }
+}
